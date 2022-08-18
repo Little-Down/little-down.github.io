b85c0e884b130f6b2d1a3c2178fe3a13ee12fd5d
Ritel Takajun <little_down@outlook.com>
2022-08-18 13:40:44 +0800

    pages

---
+++ js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+$("head").append("<link rel=\"shortcut icon\" href=\"/favicon.ico\" type=\"image/x-icon\" />")
+
+$.get('/header.html', function (data) {
+    $("#header").html(data)
+})
+$.get('/footer.html', function (data) {
+    $("#footer").html(data)
+})
